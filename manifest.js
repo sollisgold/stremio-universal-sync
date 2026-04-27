3e419c766f38e0503c0e104a893ce603e3fa0517
Sollis <126664107+sollisgold@users.noreply.github.com>
2026-04-27 10:24:32 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,70 @@
+const ADDON_ID      = 'community.stremio-universal-sync';
+const ADDON_VERSION = '1.0.0';
+
+function buildCatalogs(config) {
+  const catalogs = [];
+
+    if (!config?.options || config.options.showTrending !== false) {
+        catalogs.push(
+              { type: 'movie',  id: 'trakt:trending', name: 'Trakt Trending Movies', extra: [{ name: 'skip' }] },
+                    { type: 'series', id: 'trakt:trending', name: 'Trakt Trending Series', extra: [{ name: 'skip' }] }
+                        );
+                          }
+                            if (!config?.options || config.options.showPopular !== false) {
+                                catalogs.push(
+                                      { type: 'movie',  id: 'trakt:popular', name: 'Trakt Popular Movies', extra: [{ name: 'skip' }] },
+                                            { type: 'series', id: 'trakt:popular', name: 'Trakt Popular Series', extra: [{ name: 'skip' }] }
+                                                );
+                                                  }
+
+                                                    if (config?.traktAccessToken) {
+                                                        catalogs.push(
+                                                              { type: 'movie',  id: 'trakt:watchlist', name: 'Trakt Watchlist - Movies', extra: [{ name: 'skip' }] },
+                                                                    { type: 'series', id: 'trakt:watchlist', name: 'Trakt Watchlist - Series', extra: [{ name: 'skip' }] }
+                                                                        );
+                                                                            if (!config?.options || config.options.showRecommendations !== false) {
+                                                                                  catalogs.push(
+                                                                                          { type: 'movie',  id: 'trakt:recommendations', name: 'Trakt For You - Movies', extra: [{ name: 'skip' }] },
+                                                                                                  { type: 'series', id: 'trakt:recommendations', name: 'Trakt For You - Series', extra: [{ name: 'skip' }] }
+                                                                                                        );
+                                                                                                            }
+                                                                                                                for (const list of config?.traktLists || []) {
+                                                                                                                      const catalogId = `trakt:list:${list.username}:${list.slug}`;
+                                                                                                                            catalogs.push(
+                                                                                                                                    { type: 'movie',  id: catalogId, name: `${list.name} - Movies`, extra: [{ name: 'skip' }] },
+                                                                                                                                            { type: 'series', id: catalogId, name: `${list.name} - Series`, extra: [{ name: 'skip' }] }
+                                                                                                                                                  );
+                                                                                                                                                      }
+                                                                                                                                                        }
+                                                                                                                                                        
+                                                                                                                                                          if (config?.imdbUserId) {
+                                                                                                                                                              catalogs.push({ type: 'movie', id: 'imdb:watchlist', name: 'IMDb Watchlist', extra: [{ name: 'skip' }] });
+                                                                                                                                                                }
+                                                                                                                                                                  for (const list of config?.imdbLists || []) {
+                                                                                                                                                                      catalogs.push({ type: 'movie', id: `imdb:list:${list.id}`, name: `IMDb - ${list.name}`, extra: [{ name: 'skip' }] });
+                                                                                                                                                                        }
+                                                                                                                                                                        
+                                                                                                                                                                          return catalogs;
+                                                                                                                                                                          }
+                                                                                                                                                                          
+                                                                                                                                                                          function buildManifest(config) {
+                                                                                                                                                                            return {
+                                                                                                                                                                                id:          ADDON_ID,
+                                                                                                                                                                                    version:     ADDON_VERSION,
+                                                                                                                                                                                        name:        'Universal Sync',
+                                                                                                                                                                                            description: 'Bi-directional sync between Stremio, Trakt.tv, and IMDb. Your lists become catalogs. Your history stays in sync.',
+                                                                                                                                                                                                resources: [
+                                                                                                                                                                                                      'catalog',
+                                                                                                                                                                                                            { name: 'meta',   types: ['movie', 'series'], idPrefixes: ['tt'] },
+                                                                                                                                                                                                                  { name: 'stream', types: ['movie', 'series'], idPrefixes: ['tt'] },
+                                                                                                                                                                                                                      ],
+                                                                                                                                                                                                                          types:    ['movie', 'series'],
+                                                                                                                                                                                                                              catalogs: buildCatalogs(config),
+                                                                                                                                                                                                                                  behaviorHints: {
+                                                                                                                                                                                                                                        configurable:           true,
+                                                                                                                                                                                                                                              configurationRequired:  !config,
+                                                                                                                                                                                                                                                  },
+                                                                                                                                                                                                                                                    };
+                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                    module.exports = { buildManifest, ADDON_ID };
